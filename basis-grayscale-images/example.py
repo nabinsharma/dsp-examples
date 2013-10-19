7d8048d7a330ff331af52e215184ffa658f6943c
Nabin Sharma <nabin.s.sharma@gmail.com>
2013-10-19 12:47:22 -0400

    Post the image basis example

---
+++ basis-grayscale-images/example.py
@@ -0,0 +1,60 @@
+# Nabin Sharma
+# Oct 19, 2013
+#
+# You need pygame installed to run this program. I worked on
+# python 3.3 and there was no imread support using standard
+# PIL based modules. So I used pygame to read images.
+
+import numpy
+import pygame
+import pygame.surfarray
+import pylab
+
+import haar
+
+
+def read_image(filename):
+    surf = pygame.image.load(filename)
+    im = numpy.asmatrix(pygame.surfarray.pixels_red(surf)).T
+    return im
+
+
+def display_image(im, title=None):
+    pylab.figure()
+    pylab.imshow(im, cmap='gray')
+    pylab.axis('image')
+    pylab.xticks([])
+    pylab.yticks([])
+    pylab.title(title)
+
+
+def main():
+    I = read_image('camera.jpg')
+    display_image(I, "Original image")
+
+    # Arrange image in column vector.
+    I = I.flatten(1).T
+
+    # Generate Haar basis vector (rows of H).
+    H = numpy.asmatrix(haar.haar(len(I)))
+
+    # Project image on the new basis.
+    I_haar = H * I
+
+    # Remove the second half of the coefficient.
+    I_haar[len(I_haar)/2:, 0] = 0
+
+    # Recover the image by inverting change of basis.
+    I_haar = H.T * I_haar
+    error = I - I_haar
+    distance = numpy.sqrt(error.T * error)
+
+    # Rearrange pixels of the image.
+    I_haar = numpy.reshape(I_haar, (64, 64), 'F')
+    display_image(I_haar, "Recoevered image, distance = {}".format(distance))
+
+    pylab.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ basis-grayscale-images/haar.py
@@ -0,0 +1,21 @@
+import numpy
+
+def haar(N):
+    """
+    Returns a Haar matrix of size NxN.
+    Ref: https://spark-public.s3.amazonaws.com/dsp/num_examples/Module%203/code/haar.m
+    """
+    # TODO (Nabin): Make sure N is power of 2.
+    h = numpy.zeros((N, N))
+    h[0, :] = numpy.ones(N) / numpy.sqrt(N)
+    for k in range(1, N):
+        p = numpy.fix(numpy.log(k)/numpy.log(2))
+        q = k - 2**p
+        k1 = 2.0**p
+        t1 = N / k1 
+        k2 = 2.0**(p + 1)
+        t2 = N / k2
+        for i in numpy.arange(1, t2):
+            h[k+1, i+q*t1] = 2**(p/2) / numpy.sqrt(N)
+            h[k+1, i+q*t1+t2] = -2**(p/2) / numpy.sqrt(N)
+    return h
