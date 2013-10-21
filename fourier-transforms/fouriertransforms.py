3333ae45d2fe490ada4419782cc42e291b6d2fee
Nabin Sharma <nabin.s.sharma@gmail.com>
2013-10-20 22:41:41 -0400

    Add examples showing effects of limited base, and fft basics

--- fourier-transforms/fouriertransforms.py
+++ fourier-transforms/fouriertransforms.py
@@ -62,6 +62,10 @@ def plot_dft(X, title=None):
     pylab.xlabel("normalized frequencies")
 
 
+def _generate_sin_wave(f, N, A=1.0):
+    return A * numpy.sin(2.0 * numpy.pi * f * numpy.arange(N) / N)
+
+
 def example1():
     # Genreate signal.
     N = 128
@@ -76,7 +80,7 @@ def example1():
         X1, "DFT of x1 with numerical errors in phase hidden", True)
     # Inverse DFT (synthesize x1)
     x1_s = numpy.squeeze(numpy.asarray((
-        1.0 / N) * numpy.conj(WN) * numpy.asmatrix(X1).T))
+        1.0 / N) * WN.H * numpy.asmatrix(X1).T))
     plot_dt_signal(numpy.real(x1_s), "x1 via inverse DFT")
 
 
@@ -92,7 +96,48 @@ def example2():
     plot_dft(X, "Two sinusoids")
 
 
+def example3():
+    """Three tones."""
+    x1 = _generate_sin_wave(40, 1000)
+    x2 = _generate_sin_wave(80, 1000)
+    x3 = _generate_sin_wave(160, 1000)
+    z = numpy.zeros(1000)
+    x = numpy.hstack((x1, z, x2, z, x3))
+    N = len(x)
+    WN = dftmtx(N)
+    X = numpy.squeeze(numpy.asarray(WN * numpy.asmatrix(x).T))
+    plot_dft(X, "Three tones")
+    # Reduce base.
+    WN_reduced = numpy.delete(WN, range(600, 4400), 0)
+    X_approx = WN_reduced * numpy.asmatrix(x).T
+    x_approx = (1.0 / N) * (WN_reduced.H * X_approx)
+    X = numpy.squeeze(numpy.asarray(WN * x_approx))
+    plot_dft(X, "Approximating with limited base causes loss of one sinusoid")
+
+
+def example4():
+    """FFT"""
+    N = 4000
+    x = _generate_sin_wave(40, N) + _generate_sin_wave(80, N)
+    M = N
+    X = numpy.fft.fft(x, M)
+    plot_dft(X, "FFT of input without truncation")
+    M = 500
+    X = numpy.fft.fft(x, M)
+    plot_dft(X, "FFT of input with truncation")
+    M = 50
+    X = numpy.fft.fft(x, M)
+    plot_dft(X, "FFT of input with more truncation")
+    xM = numpy.fft.ifft(X, M)
+    M = 4000
+    X = numpy.fft.fft(xM, M)
+    plot_dft(X, "A case where increasing dft size has no other advantage "
+             "than nicer plot")
+
+
 if __name__ == "__main__":
     example1()
     example2()
+    example3()
+    example4()
     pylab.show()
