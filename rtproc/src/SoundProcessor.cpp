60ec79e870e75ad55c87a5138d934960cb5738c8
Nabin Sharma <nabin.s.sharma@gmail.com>
2013-11-08 20:57:05 -0500

    Source credits

--- rtproc/src/SoundProcessor.cpp
+++ rtproc/src/SoundProcessor.cpp
@@ -1,3 +1,13 @@
+// Digital Signal Processing (on Coursera)
+//   by Paolo Prandoni, Martin Vetterli
+//
+// Real-Time Audio Processing - a representative example
+// demonstrating baisc DSP algorithms in action - in real time!!!
+//
+// Original source code by: Paolo Prandoni, Martin Vetterli.
+//
+// Modified by: Nabin Sharma, Nov 08, 2013.
+
 #include "soundProcessor.h"
 #include <math.h>
 #include <stdio.h>
--- rtproc/src/main.cpp
+++ rtproc/src/main.cpp
@@ -1,3 +1,13 @@
+// Digital Signal Processing (on Coursera)
+//   by Paolo Prandoni, Martin Vetterli
+//
+// Real-Time Audio Processing - a representative example
+// demonstrating baisc DSP algorithms in action - in real time!!!
+//
+// Original source code by: Paolo Prandoni, Martin Vetterli.
+//
+// Modified by: Nabin Sharma, Nov 08, 2013.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
